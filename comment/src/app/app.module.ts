24afeb79fc599a466852b29cc8c370e020eb364f
zohreh-b <z.bodaghi.g79@gmail.com>
2019-09-01 12:21:56 +0430

    ورود افراد عام

--- comment/src/app/app.module.ts
+++ comment/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { FooterComponent } from './homepage/footer/footer.component';
 import { IndustryComponent } from './industry/industry.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { from } from 'rxjs';
 
 
 
@@ -34,7 +36,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- comment/src/app/login/login.component.ts
+++ comment/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+
+import{MustMatch} from './_helper/must-match.validator';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  registerForm:FormGroup;
+  submitted=false;
 
-  constructor() { }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm=this.formBuilder.group({
+    
+      firstName:['',Validators.required],
+      lastName:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6)]],
+      confirmPassword:['',Validators.required]
+      
+    },{
+      validator:MustMatch('password','confirmPassword')
+    });
+  }
+  get f(){return this.registerForm.controls;}
+  onSubmit(){
+    this.submitted=true;
+    if(this.registerForm.invalid){return;}
+    alert('SUCCESS!! :-) \n\n'+JSON.stringify(this.registerForm.value,null,4));
+  }
+  onReset(){
+    this.submitted=false;
+    this.registerForm.reset();
   }
 
 }
