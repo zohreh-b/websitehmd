318e99b27bb43aeb1a6c082ddf111ae9ca0ff89d
zohreh-b <z.bodaghi.g79@gmail.com>
2019-10-05 22:40:31 +0330

    ایجاد کامپ اشتراک اصلاح یکسری لینک ها

---
+++ comment/src/app/eshterak/eshterak.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eshterak',
+  templateUrl: './eshterak.component.html',
+  styleUrls: ['./eshterak.component.css']
+})
+export class EshterakComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
